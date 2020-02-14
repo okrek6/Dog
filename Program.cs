96acd2efe11826c9302c69a3e9b8a84186a400be
Brendan Krekeler <36323683+okrek6@users.noreply.github.com>
2020-02-13 19:26:15 -0600

    Final Commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace Dog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dog puppy = new Dog("Orion", "Shawn", 1, Gender.Male);
+            puppy.Bark(3);
+            Console.WriteLine(puppy.GetTag());
+
+            Dog myDog = new Dog("Lileu","Dale", 4, Gender.Female);
+            myDog.Bark(1);
+            Console.WriteLine(myDog.GetTag());
         }
     }
 }
